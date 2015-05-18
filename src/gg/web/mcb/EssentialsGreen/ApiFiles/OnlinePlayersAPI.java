4335be5659d72413b63b0c78e9161dd83373cc65
Marco <Marco0902@live.de>
2015-05-18 20:47:03 +0200

    OnlinePlayersApi added Feature

--- src/gg/web/mcb/EssentialsGreen/ApiFiles/OnlinePlayersAPI.java
+++ src/gg/web/mcb/EssentialsGreen/ApiFiles/OnlinePlayersAPI.java
@@ -2,11 +2,19 @@ package gg.web.mcb.EssentialsGreen.ApiFiles;
 
 import java.util.ArrayList;
 
+import org.bukkit.Bukkit;
+
 import gg.web.mcb.EssentialsGreen.MainPackage.EssentialsGreen;
 
 public class OnlinePlayersAPI {
 	public static ArrayList<String> getAllOnlinePlayers(){
 		ArrayList<String> Players = new EssentialsGreen().OnlinePlayers;
 		return Players;
+	}
+	
+	public static boolean IfPlayerOnline(String Name){
+		if(!(Bukkit.getPlayer(Name) == null)){
+			return true;
+		}else return false;
 	}
 }
